6ec1ed0d06c9a2c38dc6f4108a7d9147b20b5f7d
MOJAN3543 <sjs732@naver.com>
2026-05-09 16:35:12 +0900

    Chore: 사용하지 않는 API 삭제

--- src/hooks/api/useAnalytics.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { getFunnelAnalytics } from "../../services/analytics";
-
-export const useGetFunnelAnalytics = () => {
-  return useQuery({
-    queryKey: ["funnelAnalytics"],
-    queryFn: getFunnelAnalytics,
-  });
-};
